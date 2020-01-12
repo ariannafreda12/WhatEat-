ac26f39ccfe41d2474512298155d0c4cf8746232
ei-em <ei.em@outlook.it>
2020-01-12 22:26:33 +0100

    RICORDA! Funziona solo con il LOGIN  non con CONTINUE AS VISITOR

---
+++ WhatEatCORRECT/src/controller/RecipeManager.java
@@ -0,0 +1,41 @@
+package controller;
+
+import java.util.ArrayList;
+import dao.RecipeDao;
+import model.Recipe;
+
+public class RecipeManager {
+	
+	private static RecipeManager instance = null;
+	private Recipe recipe;//singleton
+
+    public RecipeManager() {}
+
+    public static RecipeManager getInstance() {
+
+        if (instance == null) {
+            instance = new RecipeManager();
+        }
+        return instance;
+    }
+    
+    public Recipe getRecipe() {
+        return recipe;
+     }
+
+    public void setRecipe(Recipe r){
+        recipe = r;
+    }
+     
+    public static ArrayList<Recipe> foundIngredient(ArrayList<String> ingredients,String category, String difficulty) {
+    	return RecipeDao.ingredientsDao(ingredients,category,difficulty);
+    }
+    
+    public static Recipe chooseRecipe(String title) {
+    	return RecipeDao.chooseRecipeDao(title);
+    }
+
+	public boolean reviewRecipe(String title, int review) {
+		return RecipeDao.reviewRecipeDao(title, review);
+	}
+}
